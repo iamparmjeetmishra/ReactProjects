3e95a9eb4bb50b819c7ce4d1e8efb153184fe4b2
Parmjeet <iamparmjeetmishra@gmail.com>
2024-01-27 23:40:10 +0530

    UseEffectHook - Setup

--- 00React/src/hooks/index.js
+++ 00React/src/hooks/index.js
@@ -1,9 +1,11 @@
 import UseCallbackHook from './UseCallbackHook'
 import UseLayoutHook from './UseLayoutHook'
 import UseMemoHook from './UseMemoHook'
+import UseEffectHook from './UseEffectHook'
 
 export {
     UseCallbackHook,
     UseLayoutHook,
     UseMemoHook,
+    UseEffectHook,
 }
